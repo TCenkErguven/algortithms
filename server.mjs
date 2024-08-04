9cd3d95776a39fe1ccf0d3cdac72e6e1f908167a
Cenk Ergüven <cenk.erguven@eteration.com>
2024-08-04 19:11:56 +0300

    add mergeSort and bubbleSort

--- server.mjs
+++ server.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { insertionSort, selectionSort } from "./sort.mjs";
+import { insertionSort, selectionSort, mergeSort, bubbleSort } from "./sort.mjs";
 const app = express();
 const port = 3000;
 
@@ -17,18 +17,25 @@ app.post("/sort/insertion", (req, res) => {
 
 app.post("/sort/selection", (req, res) => {
   const unsortedArray = req.body;
-  console.log("Insertion sort request...");
+  console.log("Selection sort request...");
   const sortedArray = selectionSort(unsortedArray);
   res.json(sortedArray);
 });
-/*
-  app.post("/sort/merge", (req, res) => {
-    const unsortedArray = req.body;
-    console.log("Insertion sort request...");
-    const sortedArray = mergeSort(unsortedArray);
-    res.json(sortedArray);
-  });
-*/
+
+app.post("/sort/merge", (req, res) => {
+  const unsortedArray = req.body;
+  console.log("Merge sort request...");
+  const sortedArray = mergeSort(unsortedArray);
+  res.json(sortedArray);
+});
+
+app.post("/sort/bubble", (req, res) => {
+  const unsortedArray = req.body;
+  console.log("Bubble sort request...");
+  const sortedArray = bubbleSort(unsortedArray);
+  res.json(sortedArray);
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- sort.mjs
+++ sort.mjs
@@ -34,17 +34,61 @@ export function selectionSort(unsortedArray) {
     }
 
     if (i !== lowest) {
-      [unSortedElements[i], unSortedElements[lowest]] = [unSortedElements[lowest], unSortedElements[i]]; // Swap elements
+      [unSortedElements[i], unSortedElements[lowest]] = [unSortedElements[lowest], unSortedElements[i]];
     }
   }
 
   return unSortedElements;
 }
 
-
+//for large data its better to use rather than previous two
 export function mergeSort(unsortedArray){
   const unSortedElements = [...unsortedArray];
-  const length = unSortedElements.length;
+  if(unSortedElements.length === 1)
+    return unsortedArray;
+
+  const middle = Math.floor(unSortedElements.length / 2);
+  const left  = unSortedElements.slice(0, middle);
+  const right = unSortedElements.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right){
+  let resultArray = []; 
+  let leftIndex = 0; 
+  let rightIndex = 0;
+
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] < right[rightIndex]){
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
+//Popular but it works inefficient. It works by repeatedly swapping 
+//adjacent elements that are out of order
+export function bubbleSort(unsortedArray){
+  const unSortedElements = [...unsortedArray];
+  const lengthOfElements = unSortedElements.length;
+
+  for(let i = 0; i < lengthOfElements - 1 ; i++){
+    for(let j = lengthOfElements; j >= i; j--){
+      if(unSortedElements[j] < unSortedElements[j - 1]){
+        const prev = unSortedElements[j];
+        const next = unSortedElements[j - 1];
+        unSortedElements[j] = next;
+        unSortedElements[j -1] = prev;  
+      }
+    }
+  }
 
   return unSortedElements;
 }
